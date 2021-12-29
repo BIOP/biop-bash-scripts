b4ac7a9a505bd40967a460fbd05c6e93c5647d3d
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-12-29 15:59:07 +0100

    simple qupath install script

---
+++ install_qupath.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# ----------------- COMPONENTS VERSION -----------
+qupath_version=0.3.1
+
+################################################################################
+# Help                                                                         #
+################################################################################
+function Help()
+{
+   # Display Help
+   echo ------ QuPath Installer Script -------------
+   echo "This batch file downloads and install QuPath-v$qupath_version on your computer."
+   echo
+   echo "You can specify the folder where to install QuPath as an "
+   echo "argument of this script. For instance: "
+   echo ""
+   echo "Windows:"
+   echo "./install_qupath.sh C:/"
+   echo ""
+   echo "If no path is specified, you will be asked for one."
+   echo
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+# ----------------- FUNCTIONS -------------------
+
+# Wait for user 
+function pause(){
+   read -p "$*"
+}
+
+# Returns
+function getuserdir(){
+    local  __resultvar=$1
+	local  myresult=
+		while true ; do
+			read -r -p "Path: " myresult
+			if [ -d "$myresult" ] ; then
+				break
+			fi
+			echo "$myresult is not a directory... try without slash at the end (unless it's the root drive like C:/)"
+		done
+    eval $__resultvar="'$myresult'"
+}
+
+echo ------ QuPath Installer Script -------------
+echo "This batch file downloads and install QuPath on your computer"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "Your OS: Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Your OS: Mac OSX"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "Your OS: Windows"
+else
+    echo "Unknown OS, the script will exit: $OSTYPE"
+	pause "Press [Enter] to end the script"
+	exit 1 # We cannot proceed
+fi
+
+
+# ------- INSTALLATION PATH VALIDATION
+
+echo ------- Installation path validation
+
+if [ $# -eq 0 ] 
+then
+	echo "Please enter the installation path for QuPath"
+	getuserdir path_install
+else 	
+	if [ -d "$1" ] ; then
+		path_install=$1
+	else
+		echo $1 is not a valid path
+		echo "Please enter the installation path for QuPath"
+		getuserdir path_install
+	fi	
+fi
+
+echo "All components will be installed in:"
+echo "$path_install"
+
+# MAKE TEMP FOLDER IN CASE DOWNLOADS ARE NECESSARY
+temp_dl_dir="$path_install/temp_dl"
+mkdir "$temp_dl_dir"
+
+# ------ SETTING UP QUPATH ------
+echo ------ Setting up QuPath ------
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "Linux unsupported - please contribute to this installer to support it!"
+	pause "Press [Enter] to end the script"
+	exit 1 # We cannot proceed
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Mac OSX unsupported - please contribute to this installer to support it!"
+	pause "Press [Enter] to end the script"
+	exit 1 # We cannot proceed
+elif [[ "$OSTYPE" == "msys" ]]; then
+	qupath_executable_file="QuPath-${qupath_version}.exe"
+	qupath_url="https://github.com/qupath/qupath/releases/download/v${qupath_version}/QuPath-${qupath_version}-Windows.zip"
+	qupath_path="$path_install/QuPath-${qupath_version}/$qupath_executable_file"
+	if [[ -f "$qupath_path" ]]; then
+		echo "QuPath detected, bypassing installation"
+	else
+		echo "QuPath not present, downloading it from $qupath_url"
+		qupath_zip_path="$temp_dl_dir/qupath.zip"
+		curl "$qupath_url" -L -# -o "$qupath_zip_path"
+		echo "Unzipping QuPath"
+		unzip "$qupath_zip_path" -d "$path_install"
+		if [[ -f "$qupath_path" ]]; then
+			echo "QuPath successfully installed"
+		else
+			echo "QuPath installation failed, please retry with administrator rights or install in a folder requiring less priviledge"
+			pause "Press [Enter] to end the script"
+			exit 1 # We cannot proceed
+		fi
+	fi	
+fi
+
+
+echo "Removing temporary download folder $temp_dl_dir"
+rm -r "$temp_dl_dir"
