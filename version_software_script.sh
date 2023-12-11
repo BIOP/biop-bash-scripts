7ec37c43d9c86c8bbb19e2fdc6502e19e9c288e3
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2023-12-11 10:01:54 +0100

    Update version_software_script.sh for QuPath 0.5.0

--- version_software_script.sh
+++ version_software_script.sh
@@ -3,17 +3,17 @@
 
 #Software version number
 
-qupath_version=0.4.4
-abba_extension_version=0.1.4
+qupath_version=0.5.0
+abba_extension_version=0.3.0
 elastix_version=5.0.1
-biop_extension_version=2.0.0
-cellpose_extension_version=0.7.0
-warpy_extension_version=0.2.7
-stardist_extension_version=0.4.0
-biop_omero_extension_version=0.5.0
+biop_extension_version=3.0.0
+cellpose_extension_version=0.9.0
+warpy_extension_version=0.3.0
+stardist_extension_version=0.5.0
+biop_omero_extension_version=0.8.0
 
 #QuPath arguments :
-argQuPathUserPath="defaultQuPathUserPath=\"$path_install/QuPath_Common_Data_0.4\""
-argQuPathPrefNode="quPathPrefsNode=\"io.github.qupath/0.4\""
+argQuPathUserPath="defaultQuPathUserPath=\"$path_install/QuPath_Common_Data_0.5\""
+argQuPathPrefNode="quPathPrefsNode=\"io.github.qupath/0.5\""
 argQuPathExtensionURL="quPathExtensionURL=\"$qupath_abba_extension_url\""
 argQuitAfterInstall="quitAfterInstall=\"true\""
