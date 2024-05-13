76edd9d6b642a8f1d3b04fe1fb3a4cc78e56d5cc
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-05-13 15:20:57 +0200

    Update version_software_script.sh
    Update dependencies

--- version_software_script.sh
+++ version_software_script.sh
@@ -3,15 +3,15 @@
 
 #Software version number
 
-qupath_version=0.5.0
+qupath_version=0.5.1
 abba_extension_version=0.3.1
 elastix_version=5.0.1
 biop_extension_version=3.1.0
-cellpose_extension_version=0.9.0
+cellpose_extension_version=0.9.2
 warpy_extension_version=0.3.1
 stardist_extension_version=0.5.0
-biop_omero_extension_version=0.8.0
-omero_ij_version=5.8.3
+biop_omero_extension_version=1.0.0
+omero_ij_version=5.9.0
 
 #QuPath arguments :
 argQuPathUserPath="defaultQuPathUserPath=\"$path_install/QuPath_Common_Data_0.5\""
