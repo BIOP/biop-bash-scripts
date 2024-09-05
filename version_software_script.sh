911d18abc331ca3d9c1cdb01250d447c4940820c
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-09-05 13:26:00 +0200

    Update version_software_script.sh
    Updates versions and adds url for easier check in the future

--- version_software_script.sh
+++ version_software_script.sh
@@ -3,15 +3,15 @@
 
 #Software version number
 
-qupath_version=0.5.1
-abba_extension_version=0.3.1
-elastix_version=5.0.1
-biop_extension_version=3.1.0
-cellpose_extension_version=0.9.3
-warpy_extension_version=0.3.1
-stardist_extension_version=0.5.0
-biop_omero_extension_version=1.0.0
-omero_ij_version=5.9.0
+qupath_version=0.5.1 # https://github.com/qupath/qupath/releases
+abba_extension_version=0.3.1 # https://github.com/BIOP/qupath-extension-abba/releases
+elastix_version=5.2.0 # https://github.com/SuperElastix/elastix/releases
+biop_extension_version=3.1.0 # https://github.com/BIOP/qupath-extension-biop/releases
+cellpose_extension_version=0.9.3 # https://github.com/BIOP/qupath-extension-cellpose/releases
+warpy_extension_version=0.3.1 # https://github.com/BIOP/qupath-extension-warpy/releases
+stardist_extension_version=0.5.0 # https://github.com/qupath/qupath-extension-stardist/releases
+biop_omero_extension_version=1.0.3 # https://github.com/BIOP/qupath-extension-biop-omero/releases
+omero_ij_version=5.8.4 # https://www.openmicroscopy.org/omero/downloads/ 
 
 #QuPath arguments :
 argQuPathUserPath="defaultQuPathUserPath=\"$path_install/QuPath_Common_Data_0.5\""
